aac19eea94e72e784ab050a795b5545e01006cc8
Dharmin Makrubiya <dharminmakrubiya@hotmail.com>
2022-12-19 12:26:41 +0530

    If Else Blade Example

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -8,3 +8,14 @@
 @foreach ($data as $item)
     {{$item}}<br>
 @endforeach
+
+
+
+{{-- If Else Blade syntext --}}
+@if (count($data) === 1)
+    I have one record!
+@elseif (count($data) > 1)
+    I have multiple records!
+@else
+    I don't have any records!
+@endif
